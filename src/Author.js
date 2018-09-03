0e344c254abc329a841f6aefa47c60feb88b096a
Alexander Rau <alexanderrau86@yahoo.com>
2018-09-02 18:00:51 -0600

    Finish initial build

--- src/Author.js
+++ src/Author.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import './Story.css';
 import './Author.css';
 
 
 const Author = (props) => {
+  console.log(props)
   return (
     <div className='story-author-info'>
-      <img src={props.authImg} className='story-author-image' />
+      <img src={props.author.story.authImg} className='story-author-image' />
       <div className='story-author-text'>
-        <h4>{props.authName}</h4>
-        <h4>{props.estTime} read</h4>
+        <h4>{props.author.story.authName}</h4>
+        <h4>{props.author.story.estTime} read</h4>
       </div>
     </div>
   )
--- src/Story.js
+++ src/Story.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Button from './Button';
+import Author from './Author';
 import './Story.css';
 
 const Story = (props) => {
-  console.log(props)
   return (
     <div className='Story'>
       <img src={props.story.img} />
@@ -12,8 +12,7 @@ const Story = (props) => {
           <h3>{props.story.title}</h3>
           <p>{props.story.desc}</p>
         </div>
-        {/* create an Author component and add it here */}
-        {/* it will need properties of the author image, name, and the estimated reading time of the story */}
+        <Author author={props} />
       </div>
     </div>
   )
